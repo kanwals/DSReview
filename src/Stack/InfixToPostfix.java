4faff8d7f7dd682533d34f707047c130f2a39d3d
Gurkanwal <gurkanwal.singh7@gmail.com>
2017-07-29 15:22:39 +0530

    Postfix Evaluation

--- src/Stack/InfixToPostfix.java
+++ src/Stack/InfixToPostfix.java
@@ -38,7 +38,7 @@ public class InfixToPostfix {
                         postfix += stack.pop();
                     }
                 } catch (NoSuchElementException e){
-                    throw new IllegalArgumentException("Not a valid infix Expression.");
+                    throw new IllegalArgumentException("Not a valid infix Expression. Please check bracket placements.");
                 }
                 stack.pop();
             } else {
@@ -54,13 +54,42 @@ public class InfixToPostfix {
 
         return postfix;
     }
+    private static int doOperation(int a, int b, char op){
+        switch(op){
+            case '+': return a+b;
+            case '-': return a-b;
+            case '*': return a*b;
+            case '/': return a/b;
+            case '^': return (int)Math.pow(a,b);
+        }
+        throw new IllegalArgumentException("Operator "+op+" not supported.");
+    }
+
+    public static int evaluatePostfix(String postfix){
+
+        StackLL<Integer> stack = new StackLL<Integer>();
+        for (int i=0; i<postfix.length(); i++){
+            char ch = postfix.charAt(i);
+            if(Character.isLetterOrDigit(ch)){
+                stack.push(Character.getNumericValue(ch));
+                System.out.println("--Top of stack: "+stack.peek());
+            }
+            else{
+                int a = stack.pop();
+                int b = stack.pop();
+                stack.push(doOperation(b,a,ch));
+                System.out.println("Top of stack: "+stack.peek());
+            }
+        }
+        return stack.pop();
+    }
 
     public static void main(String[] args) {
 
-        String infix = "2*(3+(4/2))-3";
+        String infix = "2*(3+(4^2))-3";
         System.out.println("Infix Expression:\t"+infix);
         System.out.println("Postfix Expression:\t"+ infixToPostfix(infix));
-
+        System.out.println("Result: "+evaluatePostfix(infixToPostfix(infix)));
     }
 }
 
